4975b6f6d200c367b790ed566f91eb22ece63752
Deena Hansika <deenahansika@gmail.com>
2025-10-13 10:35:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Non-Adjacent Nodes Sum/maximum-non-adjacent-nodes-sum.cpp
@@ -0,0 +1,31 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+     pair<int, int> solve(Node* root) {
+        if (!root) return {0, 0};
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+        int include = root->data + left.second + right.second;
+        int exclude = max(left.first, left.second) + max(right.first, right.second);
+        return {include, exclude};
+    }
+    int getMaxSum(Node *root) {
+        // code here
+        auto hansi=solve(root);
+        return max(hansi.first,hansi.second);
+        
+    }
+};
